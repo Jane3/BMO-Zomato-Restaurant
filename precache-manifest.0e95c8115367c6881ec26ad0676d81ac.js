c384bd73490f9648d63dddd7382ab208067991eb
Jane You <janeyou@Janes-MacBook-Pro.local>
2021-03-26 14:25:45 -0400

    Updates

---
+++ precache-manifest.0e95c8115367c6881ec26ad0676d81ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8891d949bdd3e247100ddc1cd36b164",
+    "url": "/BMO-Zomato-Restaurant/index.html"
+  },
+  {
+    "revision": "caa5f0736496ce1a5dde",
+    "url": "/BMO-Zomato-Restaurant/static/css/main.f79e5c9c.chunk.css"
+  },
+  {
+    "revision": "a50cf5b32b3b8e3cd3ba",
+    "url": "/BMO-Zomato-Restaurant/static/js/2.bea50254.chunk.js"
+  },
+  {
+    "revision": "1ae01fb0d60dfdefe7f9d6cbefaaa517",
+    "url": "/BMO-Zomato-Restaurant/static/js/2.bea50254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa5f0736496ce1a5dde",
+    "url": "/BMO-Zomato-Restaurant/static/js/main.84f8c3c5.chunk.js"
+  },
+  {
+    "revision": "65a2b22079a6065667eb",
+    "url": "/BMO-Zomato-Restaurant/static/js/runtime-main.a723e2f8.js"
+  }
+]);
